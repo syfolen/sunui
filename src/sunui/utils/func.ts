a46c72a143cc347d54ba3a74347a498c43af563f
syfolen <syfolen@163.com>
2020-06-11 19:35:35 +0800

    111

--- src/sunui/utils/func.ts
+++ src/sunui/utils/func.ts
@@ -5,12 +5,12 @@ module sunui {
      * 在对象或子对象中查找
      * export
      */
-    export function find(path: string, parent: Laya.Node): any {
+    export function find<T>(path: string, parent: Laya.Node): T {
         const array: string[] = path.split("/");
         while (parent != null && array.length > 0) {
             const name: string = array.shift();
             parent = parent.getChildByName(name);
         }
-        return parent;
+        return parent as any;
     }
 }
